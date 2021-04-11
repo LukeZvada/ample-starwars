f391a74d733c0f76b8738a9ddf6e491ead3c4db2
LukeZvada <zvadamusic@gmail.com>
2021-04-10 17:49:06 -0700

    refactored code to use less of it. Added handleKeypress and handleONSubmitSearch funstions to handle state of search. Currently wip. Listed out to-do comments.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,82 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from "react"
 
-export const CharacterSearch = ({character}) => { 
-  console.log(character)
+  //Server Side rendering 
+const defaultEndpoint = 'https://swapi.dev/api/people/'
+
+export const getServerSideProps = async pageContext => {
+  const apiResponse = await fetch(defaultEndpoint)
+  //turn response into json 
+  const character = await apiResponse.json()
+    
+  return {
+    props: {
+      character
+    }
+  }
+}
+
+export const CharacterSearch = ({ character }) => { 
+  // console.log(character)
+  const [characterQuery, setCharacterQuery] = useState('')
+  const [characterResults, setCharacterResults] = useState([])
+  const [page, updatePage] = useState({
+    current: defaultEndpoint
+  })
+  const {current} = page 
+
+  useEffect(() => {
+    if (current === defaultEndpoint ) return; 
+
+    async function request () { 
+      const res = await fetch(current)
+      const searchResponse = await res.json();
+
+      updatePage({
+        current,
+        ...searchResponse.info
+      })
+
+      // check searchResults.count to determine that value is greater than 0
+      setCharacterResults(searchResponse)
+      // if count = 0, show 'no results' message
+      // if count > 0, setCharacterResults to the array of results
+      // update return to accommodate multiple results 
+
+    }
+    request();
+  }, [current]);
+  
+  const handleKeyPress = (event) => {
+    setCharacterQuery(event.target.value)
+    console.log(event.target.value)
+  }
+
+  const handleOnSubmitSearch = (e) => { 
+    e.preventDefault();
+
+    const endpoint = `https://swapi.dev/api/people/?search=${characterQuery}`
+
+    updatePage({
+      current: endpoint
+    })
+    console.log(characterQuery)
+  }
+
   return (
     <>
       <article className='main-container'>
           <h1>STARWARS</h1>
+          <form className='search' onSubmit={handleOnSubmitSearch}>
+            <input onKeyDown={handleKeyPress} 
+              name="name"
+              type="search" 
+              placeholder="Search for a character" />
+          </form>
+          {/* first, check if characterResults.count = 0 and if so, show "no results message" */}
+          {/* if characterResults.count > 0
+              loop over characterResults.results */}
           <section className="character-info">
             <h1 className='character-name'>{character.name}</h1> 
               <div className='about-me'>
@@ -37,18 +107,5 @@ export const CharacterSearch = ({character}) => {
   )
 }
 
-export const getServerSideProps = async pageContext => {
-  const apiResponse = await fetch(
-    'https://swapi.dev/api/people/1'
-  )
-  //turn response into json 
-  const character = await apiResponse.json()
-
-  return {
-    props: {
-      character
-    }
-  }
-}
 
 export default CharacterSearch
