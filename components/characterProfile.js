fe0b0b60dcee07ca5541ee92d8bd961264d21e7c
LukeZvada <zvadamusic@gmail.com>
2021-04-15 20:25:38 -0700

    refactored if else in character profile to render the noResultsList correctly.

--- components/characterProfile.js
+++ components/characterProfile.js
@@ -10,49 +10,55 @@ export const CharacterProfile = ({  }) => {
     const { searchResults } = useContext(CharacterContext)
     console.log(searchResults)
     //main character profile component that renders the character "about me" data. Also rendering the filmList, starshipList, and speciesList components
-    return (
-    <>
-        {
-            searchResults.length === 1 ?
-                searchResults.map(character => {
-                    return <section key={character.name} className={styles.listContainer}>
-                                <div className={styles.listSection}>
-                                    <h1 className={styles.characterName}> {character.name} </h1>
-                                    <h2 className={styles.listTitle}>About </h2>
-                                        <ol className={styles.aboutList}>
-                                            <li>
-                                                <label className={styles.formLabel}>Height:</label>
-                                                <span>{character.height}</span>
-                                            </li>
-                                            <li>
-                                                <label className={styles.formLabel}>Weight:</label>
-                                                <span>{character.mass}</span>
-                                            </li>
-                                            <li>
-                                                <label className={styles.formLabel}>Hair Color:</label> 
-                                                <span>{character.hair_color}</span>
-                                            </li>
-                                            <li>
-                                                <label className={styles.formLabel}>Date of Birth:</label> 
-                                                <span>{character.birth_year}</span></li>
-                                            <SpeciesList />
-                                        </ol>
-                                </div>
-                                <FilmList />
-                                <StarshipList />
-                            </section>
-            }) 
-            : 
-            searchResults.map(character => {
-               return <div className={styles.noResultListSection}>
-                        <h1 className={styles.noResultsCharacterName}> {character.name} </h1>
-                    </div>
-            
-        })
-        
-    }
+    if (searchResults.length === 1) {
+        return (
+            <>
+                {
+                    searchResults.map(character => {
+                        return <section key={character.name} className={styles.listContainer}>
+                                    <div className={styles.listSection}>
+                                        <h1 className={styles.characterName}> {character.name} </h1>
+                                        <h2 className={styles.listTitle}>About </h2>
+                                            <ol className={styles.aboutList}>
+                                                <li>
+                                                    <label className={styles.formLabel}>Height:</label>
+                                                    <span>{character.height}</span>
+                                                </li>
+                                                <li>
+                                                    <label className={styles.formLabel}>Weight:</label>
+                                                    <span>{character.mass}</span>
+                                                </li>
+                                                <li>
+                                                    <label className={styles.formLabel}>Hair Color:</label> 
+                                                    <span>{character.hair_color}</span>
+                                                </li>
+                                                <li>
+                                                    <label className={styles.formLabel}>Date of Birth:</label> 
+                                                    <span>{character.birth_year}</span></li>
+                                                <SpeciesList />
+                                            </ol>
+                                    </div>
+                                    <FilmList />
+                                    <StarshipList />
+                                </section>
+                    }) 
+                }
             </>
-    )
+        )
+    } else {
+        return (
+            <>
+                <h1>Your search resulted in multiple results. Please use the list below for reference and search again!</h1>
+                    {
+                        searchResults.map(character => {
+                            return <div className={styles.noResultsListSection}>
+                                        <h2 className={styles.noResultsCharacterName}> {character.name} </h2>
+                                    </div>
+                            }) 
+                    }
+            </>
+        )
+    }
 }
 
 
